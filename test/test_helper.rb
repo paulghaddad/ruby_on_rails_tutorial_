ff62184a84049d4c1b2a8c95763bd4e796b2095e
paulh16 <paulh16@gmail.com>
2014-12-05 13:26:22 -0600

    Finish user edit, update, index and destroy actions

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -24,7 +24,7 @@ class ActiveSupport::TestCase
                                   password: password,
                                   remember_me: remember_me }
     else
-      sesion[:user_id] = user.id
+      session[:user_id] = user.id
     end
   end
 
