64d8c4e884bdfa7ad39223495abaa75b1e8c6061
paulh16 <paulh16@gmail.com>
2014-12-12 16:46:23 -0600

    Add user following

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -6,6 +6,12 @@ class Micropost < ActiveRecord::Base
   validates :content, presence: true, length: { maximum: 140 }
   validate :picture_size
 
+  def Micropost.from_users_followed_by(user)
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+    where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: user)
+  end
+
   private
 
   def picture_size
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -71,4 +71,33 @@ class UserTest < ActiveSupport::TestCase
       @user.destroy
     end
   end
+
+  test "should follow and unfollow a user" do
+    paul = users(:paul)
+    archer = users(:archer)
+    assert_not paul.following?(archer)
+    paul.follow(archer)
+    assert paul.following?(archer)
+    assert archer.followers.include?(paul)
+    paul.unfollow(archer)
+    assert_not paul.following?(archer)
+  end
+
+  test "feed should have the right posts" do
+    paul = users(:paul)
+    archer = users(:archer)
+    lana = users(:lana)
+    # Posts from followed user
+    lana.microposts.each do |post_following|
+      assert paul.feed.include?(post_following)
+    end
+    # Posts from self
+    paul.microposts.each do |post_self|
+      assert paul.feed.include?(post_self)
+    end
+    # Posts from unfollowed user
+    archer.microposts.each do |post_unfollowed|
+      assert_not paul.feed.include?(post_unfollowed)
+    end
+  end
 end
